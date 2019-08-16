1cf98c3aa6faf0db1f3c76f7dd44440d95061457
JosuSotz <josusotz@gmail.com>
2019-08-16 11:46:53 -0500

    New main with more options

--- main.py
+++ main.py
@@ -2,12 +2,31 @@ import matplotlib.pyplot as plt
 from sympy import *
 import matplotlib
 import numpy as np
+import sys
 xf=0.1
 yf=1.0
 zf=10.0
-with open('formulae.dat') as f:
-    formulae=f.readlines()
-    formulae_str=formulae[0]
+file_flag = False
+formulae_str = 'x'
+equation_flag=False
+n=len(sys.argv)
+for i in range(n):
+    if '-f' in sys.argv[i]:
+        file_flag =True
+        filename=sys.argv[i+1]
+    if '-e' in sys.argv[i]:
+        equation_flag =True
+        formulae_str=sys.argv[i+1]    
+    
+if file_flag:
+    with open('formulae.dat') as f:
+        formulae=f.readlines()
+        formulae_str=formulae[0]
+        print("Using archive mode with "+formulae_str)
+if equation_flag:
+    print("Using equation "+formulae_str)
+
+      
 x = Symbol("x")
 y=sympify(formulae_str)
 yprime=y.diff(x)
@@ -15,7 +34,6 @@ f=lambdify(x,y,"numpy")
 
 range=range(7)
 
-      
 xl=[]
 yl=[]
 zl=[]
