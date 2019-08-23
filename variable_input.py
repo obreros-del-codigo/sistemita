3841f9eee19cedf33848ea2502c0e05b582a8b7f
baomass <baomassu@gmail.com>
2019-08-23 11:59:24 -0500

    inputs

--- variable_input.py
+++ variable_input.py
@@ -18,26 +18,40 @@ def foo_input():
             equation_flag =True
             formulae_str=sys.argv[i+1]
             inputs.append(formulae_str)
+#        else:
+#            raise TypeError "no hay ecuacion"
         if '-a' in sys.argv[i]:
             equation_flag =True
             a=sys.argv[i+1]
             inputs.append(a)
+        else:
+            inputs.append(1)
         if '-b' in sys.argv[i]:
             equation_flag =True
             b=sys.argv[i+1]
             inputs.append(b)
+        else:
+            inputs.append(1)
+            
         if '-x0' in sys.argv[i]:
             equation_flag =True
             x0=sys.argv[i+1]
             inputs.append(x0)
+        else:
+            inputs.append(0.1)
         if '-i' in sys.argv[i]:
             equation_flag =True
             it=sys.argv[i+1]
             inputs.append(it)
+        else:
+            inputs.append(10)
         if '-n' in sys.argv[i]:
             equation_flag =True
             n=sys.argv[i+1]
             inputs.append(n)
+        else:
+            inputs.append(2)
+            
             
 
     if file_flag:
@@ -47,6 +61,6 @@ def foo_input():
             print("Using archive mode with "+formulae_str)
     if equation_flag:
         print("Using equation ",formulae_str,"with a:",a,",b:",b,"n:",n,", initial conditions:",x0,", iterations:",it)
-        
+    #a,b,x0,i,n
     return inputs 
     
