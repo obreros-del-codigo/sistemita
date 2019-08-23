27da8b118a0c1171a9dd7aa1f2bb1abeebfcc96c
JosuSotz <josusotz@gmail.com>
2019-08-23 11:49:36 -0500

    inputs

--- variable_input.py
+++ variable_input.py
@@ -1,6 +1,7 @@
-import sys
+import main
 
 def foo_input():
+    print("here")
     xf=0.1
     yf=1.0
     zf=10.0
@@ -8,14 +9,36 @@ def foo_input():
     formulae_str = 'x'
     equation_flag=False
     n=len(sys.argv)
-
+    inputs=[]
     for i in range(n):
         if '-f' in sys.argv[i]:
             file_flag =True
             filename=sys.argv[i+1]
         if '-e' in sys.argv[i]:
             equation_flag =True
-            formulae_str=sys.argv[i+1]    
+            formulae_str=sys.argv[i+1]
+            inputs.append(formulae_str)
+        if '-a' in sys.argv[i]:
+            equation_flag =True
+            a=sys.argv[i+1]
+            inputs.append(a)
+        if '-b' in sys.argv[i]:
+            equation_flag =True
+            b=sys.argv[i+1]
+            inputs.append(b)
+        if '-x0' in sys.argv[i]:
+            equation_flag =True
+            x0=sys.argv[i+1]
+            inputs.append(x0)
+        if '-i' in sys.argv[i]:
+            equation_flag =True
+            it=sys.argv[i+1]
+            inputs.append(it)
+        if '-n' in sys.argv[i]:
+            equation_flag =True
+            n=sys.argv[i+1]
+            inputs.append(n)
+            
 
     if file_flag:
         with open('formulae.dat') as f:
@@ -23,7 +46,7 @@ def foo_input():
             formulae_str=formulae[0]
             print("Using archive mode with "+formulae_str)
     if equation_flag:
-        print("Using equation "+formulae_str)
+        print("Using equation ",formulae_str,"with a:",a,",b:",b,"n:",n,", initial conditions:",x0,", iterations:",it)
         
     return inputs 
     
