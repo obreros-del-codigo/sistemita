184b238bc1b72dabea783b8688c03482bfe3f877
baomass <baomassu@gmail.com>
2019-08-29 15:13:55 -0500

    main

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ import matplotlib.pyplot as plt
 from sympy import *
 import matplotlib
 import numpy as np
+
 import sys
 from plot import plotting
 from variable_input import *
 from iterations import *
+from graph import *
 
 quererplot=input("¿Quieres que se guarde una foto de tu ecuación? [y/n]") 
 
-inputs=foo_input()
-iterations=foo_iterations()
+inputs,formulae_str=foo_input()
+print(inputs)
+print(formulae_str)
 
-graph()
+n,a,b,x0,i=inputs
+
+
+z,x,y=foo_iterations(a,b,x0,i,n,formulae_str)
+
+foo_graph(z,y,x)
 
 
